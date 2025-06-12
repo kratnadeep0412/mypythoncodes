920cf696b9bcdcc7a8ec6b0451a544b28f7a9254
kratnadeep0412 <115572076+kratnadeep0412@users.noreply.github.com>
2025-06-12 19:19:57 +0530

    Create List-Indexing-Access.py

---
+++ List-Indexing-Access.py
@@ -0,0 +1,17 @@
+# Python Program: Accessing List Elements using Indexing
+# List of Programming Languages
+prog_lang = ["java", "python", "c++", "SQL", "swift"]
+# Accessing using positive indexing
+print("Accessing list elements using +ve indexing:")
+print(prog_lang[0])  
+print(prog_lang[1])  
+print(prog_lang[2])  
+print(prog_lang[3]) 
+print(prog_lang[4])  
+# Accessing using negative indexing
+print("\nAccessing list elements using -ve indexing:")
+print(prog_lang[-5]) 
+print(prog_lang[-4])  
+print(prog_lang[-3])  
+print(prog_lang[-2])  
+print(prog_lang[-1])  
