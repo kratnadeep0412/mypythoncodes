e80c8b992debde7830abc9610bff82439407e0b9
kratnadeep0412 <115572076+kratnadeep0412@users.noreply.github.com>
2025-06-12 21:12:46 +0530

    Create string_operations_and_membership.py

---
+++ string_operations_and_membership.py
@@ -0,0 +1,50 @@
+# String Declarations
+Str1 = 'Helo'
+Str2 = 'Good afternoon'
+Str3 = '''
+I'm Learning Python
+Today's concept is strings
+'''
+Str4 = """Strings are collection or 
+sequence of characters
+"""
+Str5 = "I'm fond of Python programming"
+Str6 = 'Everyone will say that "Python" is easy to learn'
+# Printing strings and their types
+print(Str1)
+print(type(Str1))
+
+print(Str2)
+print(type(Str2))
+
+print(Str3)
+print(type(Str3))
+
+print(Str4)
+print(type(Str4))
+
+print(Str5)
+print(type(Str5))
+
+print(Str6)
+print(type(Str6))
+# Membership operation in List
+List = ['Hi', 'Hello', 'Python', 'Java', 10, 20, 30, 40.2, 10 + 3]
+print('Python' in List)  
+print('Java' in List)    
+print('java' in List)    
+print(10 in List)        
+# Membership operation in Strings
+St1 = "Welcome to India"
+print("to" in St1)      
+
+St2 = "Welcome top India"
+print("to" in St2)      
+
+St3 = "Welcome to India"
+print("subs" in St3)     
+
+# Using 'not in' for string membership check
+print("to" not in St1)   
+print("to" not in St2)   
+print("subs" not in St3) 
