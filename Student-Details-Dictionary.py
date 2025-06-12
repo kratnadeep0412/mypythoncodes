7b4d3b4f8a65dcc3d0701e245875f9c7ce9a8722
kratnadeep0412 <115572076+kratnadeep0412@users.noreply.github.com>
2025-06-12 18:36:49 +0530

    Create Student-Details-Dictionary.py

---
+++ Student-Details-Dictionary.py
@@ -0,0 +1,16 @@
+# Python Program: Student Details Entry Using Dictionary
+# Creating an empty dictionary
+Student_details = {}
+# Collecting student information
+Student_details['First Name'] = input("Enter First Name: ")
+Student_details['Last Name'] = input("Enter Last Name: ")
+Student_details['Gender'] = input("Enter Gender: ")
+Student_details['Contact Number'] = input("Enter Contact Number: ")
+Student_details['Date of Birth'] = input("Enter Date of Birth (DD/MM/YYYY): ")
+Student_details['Highest Qualification'] = input("Enter Highest Qualification: ")
+Student_details['Current Ongoing Degree'] = input("Enter Current Ongoing Degree: ")
+Student_details['Mail ID'] = input("Enter Mail ID: ")
+Student_details['Password'] = input("Enter Password: ")
+# Displaying the collected student details
+print("\nStudent Details Collected:")
+print(Student_details)
