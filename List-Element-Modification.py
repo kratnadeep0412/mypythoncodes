6934cad8c910efd073ac495ddd061177c059eaee
kratnadeep0412 <115572076+kratnadeep0412@users.noreply.github.com>
2025-06-12 20:50:37 +0530

    Create List-Element-Modification.py

---
+++ List-Element-Modification.py
@@ -0,0 +1,7 @@
+# Python Program: Modifying Elements in a List
+# Initial List
+List = ['black', 'green', 'red', 'yellow', 'white']
+print("Original List:", List)
+# Modifying the element at index 4
+List[4] = 'Grey'
+print("After Modification:", List)
