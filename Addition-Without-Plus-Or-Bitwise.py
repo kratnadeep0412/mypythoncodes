55179b872be83890faaf60815974ed6a627d84b4
kratnadeep0412 <115572076+kratnadeep0412@users.noreply.github.com>
2025-06-12 18:23:49 +0530

    Create Addition-Without-Plus-Or-Bitwise.py

---
+++ Addition-Without-Plus-Or-Bitwise.py
@@ -0,0 +1,9 @@
+#addition without '+' operator
+Num1 = int(input("Enter the number:"))
+Num2 = int(input("Enter the number:"))
+Sum=Num1-(-Num2)
+print(Sum)
+#addition with bitwise operator
+Num1 = int(input("Enter the number:"))
+Num2 = int(input("Enter the number:"))
+print(Num1^Num2)
