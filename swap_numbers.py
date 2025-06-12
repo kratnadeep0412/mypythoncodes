41afb281f56ed2e35cf8c13eac08b16118ebbb19
kratnadeep0412 <115572076+kratnadeep0412@users.noreply.github.com>
2025-06-12 21:20:54 +0530

    Create  swap_numbers.py

---
+++ swap_numbers.py
@@ -0,0 +1,12 @@
+# Swapping two numbers without using a temporary variable
+Num1 = int(input("Enter the first number: "))
+Num2 = int(input("Enter the second number: "))
+print(f"Num1 = {Num1}")
+print(f"Num2 = {Num2}"
+print("After swapping:")
+# Swapping without using a third variable
+Num1 = Num1 + Num2
+Num2 = Num1 - Num2
+Num1 = Num1 - Num2
+print(f"Num1 = {Num1}")
+print(f"Num2 = {Num2}")
