ddbfb3bec3fe09253d18561ca9cf98552ec35187
kratnadeep0412 <115572076+kratnadeep0412@users.noreply.github.com>
2025-06-12 20:57:45 +0530

    Create List-Slicing-Python.py

---
+++ List-Slicing-Python.py
@@ -0,0 +1,7 @@
+# Python Program to Demonstrate List Slicing
+List = [10, 20, 30, 40, 50, 60, 70, 80]
+print(List)              
+print(List[::])          
+print(List[1:4:1])      
+print(List[::2])        
+print(List[::-1])       
